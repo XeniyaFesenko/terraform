0613713d4ec76d58ff3b2e4e7aa7aca9f3988475
XeniyaFesenko <48219240+XeniyaFesenko@users.noreply.github.com>
2022-11-30 14:42:16 -0500

    Add files via upload

---
+++ HelloTerraform/main.tf
@@ -0,0 +1,84 @@
+terraform {
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> 3.0"
+     }
+   }
+ }
+
+
+provider "aws" {
+  # access_key = "$"
+  # secret_key = "$"
+  region     = "us-east-1"
+}
+
+
+#####################################  Create Bucket 
+resource "aws_s3_bucket" "state_backup" {
+  bucket = "arvindtfstate-bkp"
+  force_destroy = true
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Enable/Disable Encryption
+resource  "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+  bucket = aws_s3_bucket.state_backup.bucket
+    rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm  = "AES256"
+        }    
+  }
+}
+
+# Enable Versioning 
+resource "aws_s3_bucket_versioning" "versioning_example" {
+   bucket = aws_s3_bucket.state_backup.id
+   versioning_configuration {
+    status =   "Enabled" # | "Suspended" 
+  }
+}
+
+
+#################### Create Dynamodb Table #################
+
+# resource "aws_dynamodb_table" "terraform-lock" {
+#   hash_key = "LockID"
+#   name =  "terraform-test-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   attribute {
+#     type =  "S"
+#     name = "LockID"
+#   }
+
+# }
+
+############### Upload state file in s3 bucket with state lock 
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket= "terraformstae"
+    dynamodb_table = "terraformstate"
+    key = "xenia-tfstate/terraformstate"
+    region = "us-east-1"
+    
+  }
+} 
+
+
+?
+
+
+
+resource "aws_ecr_repository" "tf-ecr" {
+  name                 = "terraform-docker"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
